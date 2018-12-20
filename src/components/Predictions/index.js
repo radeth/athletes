af2a8462bfaa46492d0c5e3c4139689019e79d41
radeth <123radeth@gmail.com>
2018-12-21 00:37:06 +0100

    sort and filtering works

--- src/components/Predictions/index.js
+++ src/components/Predictions/index.js
@@ -4,84 +4,107 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { disciplineScore } from '../../libs/calculate'
 import './index.scss'
+import { disciplineScore } from '../../libs/calculate'
 
 export default class Predictions extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            type: 'all',
-            sort: 'alphabetical',
-            array: this.array().sort((a, b) => {
-                return a.name.localeCompare(b.name)})
+            filterType: 'all',
+            sortType: 'alphabetical',
+            arrayOfDisciplines: []
         }
-        this.array = this.array.bind(this)
+        
+    }
+    componentDidUpdate() {
+     
+   
     }
     render() {
+        this.array()
         return (
             <section className="l-section c-predictions" >
                 <h2 className="header">Predictions</h2>
                 <label>filter</label>
-                <select value={this.state.value} onChange={this.filter.bind(this)}>
+                <select value={this.state.value} onChange={this.setFilterType.bind(this)}>
                     <option>all</option>
                     <option>team</option>
                     <option>individual</option>
                 </select>
                 <label>sort</label>
-                <select value={this.state.value} onChange={this.sort.bind(this)}>
+                <select value={this.state.value} onChange={this.setSortType.bind(this)}>
                     <option>alphabetical</option>
                     <option>score</option>
                 </select>
                 <div className="content">
-                   {this.renderContent(this.state.type,this.state.sort)}
+                {this.state.arrayOfDisciplines.map((discipline)=>{
+                    return(
+                        <div key={discipline.name} className="c-discipline">
+                        <span className="name">{discipline.name}</span> - <span className="score">{disciplineScore(this.props.athlete.skillset, discipline.requirements)}</span>
+                    </div>
+                    )
+                })}
+
                 </div>
             </section>
         )
     }
-    renderContent(){
-       
-    }
-    sort(e) {
-        this.setState({
-            sort: e.target.value
-        })
-        if(e.target.value==='alphabetical'){
-            this.setState({
-                array: this.state.array.sort((a, b) => {
-                    return a.name.localeCompare(b.name)})
-            })
-        }
-        if(e.target.value==='score'){
-            this.setState({
-                array: this.state.array.sort((a, b) => {
-                    return a.score-b.score})
-            })
-        }
-        
-
-       
-    }
-    filter(e) {
-        this.setState({
-            type:  e.target.value
-        })
-    }
-    array(){
-        var disciplinesArray = []
-        let counter = 0
-        this.props.disciplines.map((discipline) =>{
-            disciplinesArray[counter] = {
+    //returning sort and filter array of disciplines
+    array(e) {
+        let disciplinesArray = this.props.disciplines
+        disciplinesArray = this.props.disciplines.map((discipline) => {
+            return discipline = {
                 ...discipline,
                 score: disciplineScore(this.props.athlete.skillset, discipline.requirements)
             }
-            counter++
+           
         })
-        return disciplinesArray
-    }
-   
-    
+        switch (this.state.sortType) {
+            case 'alphabetical':
+                disciplinesArray = disciplinesArray.sort((a, b) => {
+                    return a.name.localeCompare(b.name)
+                })
+                break
+            case 'score':
+                disciplinesArray = disciplinesArray.sort((a, b) => {
+                    return a.score - b.score
+                })
+                break
+            default:
 
+
+        }
+        switch (this.state.filterType) {
+            case 'team':
+                disciplinesArray = disciplinesArray.filter((discipline) => {
+                    return discipline.isIndividual === false
+                })
+                break
+            case 'individual':
+                disciplinesArray = disciplinesArray.filter((discipline) => {
+                    return discipline.isIndividual === true
+                })
+                break
+            default:
+        }
+       this.setState({
+           arrayOfDisciplines: disciplinesArray
+       })
+    }
+
+    //setting sort type
+    setSortType(e) {
+        this.setState({
+            sortType: e.target.value
+        })
+    }
+    //setting filter type
+    setFilterType(e) {
+       this.setState({
+           filterType: e.target.value
+       })
+    }
 }
 
 Predictions.propTypes = {
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,76 +8,77 @@ import Overview from '../../components/Overview';
 import Predictions from '../../components/Predictions';
 import Profile from '../../components/Profile';
 import './index.scss';
-import {Tabs, Tab} from '../../../node_modules/react-bootstrap' 
+import { Tabs, Tab } from '../../../node_modules/react-bootstrap'
+
 
 export default class Home extends React.Component {
     constructor() {
         super();
         this.state = {
-            counter:0
+            counter: 0
         };
         // console.log(this.props.athlete)
-      }
-    
+    }
+
     render() {
         const athlete = this.props.athletes ? this.props.athletes[this.state.counter] : null
         const disciplines = this.props.disciplines ? this.props.disciplines : []
         if (athlete)
             return (
                 <div className="p-home">
-                 <button className="btn" onClick={this.decrement.bind(this)}>prev</button>
-                 <button className="btn" onClick={this.increment.bind(this)}>next</button>
+                    <button className="btn" onClick={this.decrement.bind(this)}>prev</button>
+                    <button className="btn" onClick={this.increment.bind(this)}>next</button>
                     <Profile {...athlete} />
                     <Tabs defaultActiveKey={1} id="uncontrolled-tab">
-                    <Tab eventKey={1} title="Overview">
-                        <Overview {...athlete} />
-                    </Tab>
-                    <Tab eventKey={2} title="Predictions">
-                        <Predictions athlete={athlete} disciplines={disciplines} />
-                    </Tab>
-                    <Tab eventKey={3} title="Hints">
-                        <Hints athlete={athlete} disciplines={disciplines} />
-                    </Tab>
+                        <Tab eventKey={1} title="Overview">
+                            <Overview {...athlete} />
+                        </Tab>
+                        <Tab eventKey={2} title="Predictions">
+                            <Predictions athlete={athlete} disciplines={disciplines} />
+                        </Tab>
+                        <Tab eventKey={3} title="Hints">
+                            <Hints athlete={athlete} disciplines={disciplines} />
+                        </Tab>
                     </Tabs>
                 </div>
 
             )
-        else 
+        else
             return <div>
-                 <span>No athlete data</span>
-                </div>
-            
-    } 
-      decrement() {
+                <span>No athlete data</span>
+            </div>
+
+    }
+    decrement() {
         let counterTemp
-        if(this.state.counter===0){
+        if (this.state.counter === 0) {
             counterTemp = 0
         }
-        else{
-            counterTemp = this.state.counter - 1 
+        else {
+            counterTemp = this.state.counter - 1
         }
-       
-        this.setState({
-            counter: counterTemp
-        })
-        
 
-      }
-      increment() {
-      
+        this.setState({
+            counter: counterTemp
+        })
+
+
+    }
+    increment() {
+
         let counterTemp
-        if(this.state.counter===Object.keys(this.props.athletes).length-1){
-            counterTemp = Object.keys(this.props.athletes).length-1
+        if (this.state.counter === Object.keys(this.props.athletes).length - 1) {
+            counterTemp = Object.keys(this.props.athletes).length - 1
         }
-        else{
-            counterTemp = this.state.counter + 1 
+        else {
+            counterTemp = this.state.counter + 1
         }
         this.setState({
             counter: counterTemp
         })
     }
-      
-    }
+
+}
 
 
 
