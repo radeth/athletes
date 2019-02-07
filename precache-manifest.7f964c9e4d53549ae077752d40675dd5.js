1de188a1672c79f935a26f2f7d06efdb8725e736
radeth <123radeth@gmail.com>
2019-02-07 23:23:14 +0100

    Updates

---
+++ precache-manifest.7f964c9e4d53549ae077752d40675dd5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "371459004b67d690ea08573f5bb74864",
+    "url": "/athletes/static/media/upperBody.37145900.png"
+  },
+  {
+    "revision": "c6be76f884684a97f918a5aa1b12de67",
+    "url": "/athletes/static/media/lowerBody.c6be76f8.png"
+  },
+  {
+    "revision": "487ff71d1b197522c55f61a589fc3411",
+    "url": "/athletes/static/media/endurance.487ff71d.png"
+  },
+  {
+    "revision": "fbb0c60ce3118f95fc90f6e3a6ceadd2",
+    "url": "/athletes/static/media/agility.fbb0c60c.png"
+  },
+  {
+    "revision": "6ee46a4649d599454b4db0c7783c8d3c",
+    "url": "/athletes/static/media/aesthetics.6ee46a46.png"
+  },
+  {
+    "revision": "810989589ee46424734a",
+    "url": "/athletes/static/js/runtime~main.81098958.js"
+  },
+  {
+    "revision": "6ccc62430ac95c799956",
+    "url": "/athletes/static/js/main.6ccc6243.chunk.js"
+  },
+  {
+    "revision": "807076a70fefa06f2d15",
+    "url": "/athletes/static/js/1.807076a7.chunk.js"
+  },
+  {
+    "revision": "6ccc62430ac95c799956",
+    "url": "/athletes/static/css/main.f0bea80f.chunk.css"
+  },
+  {
+    "revision": "c70e7a61f370ec7cef2090bd212dac21",
+    "url": "/athletes/index.html"
+  }
+];
